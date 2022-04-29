bfd1acddd23c3c884594f8d6c86e9582da321bca
Kaijam Jeffers <faceless128@gmail.com>
2022-04-28 23:13:05 -0400

    added server config

---
+++ config/connection.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2');
+var dotenv = require('dotenv');
+
+// enviromnemt variables for database
+dotenv.config();
+var mysqlPass = process.env.DB_PASS;
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME
+    },
+);
+
+module.exports = db;
