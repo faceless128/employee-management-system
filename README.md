506279dc9da7a686b9b3ed05350113973755d5aa
Kaijam Jeffers <faceless128@gmail.com>
2022-04-28 22:56:10 -0400

    worked on database structure

---
+++ README.md
@@ -0,0 +1,28 @@
+# Employee Management System
+
+## Description
+A clean command-line application for managing an employee database.
+
+  ## Table of Contents
+* [Features](#features)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Tests](#tests)
+* [Demonstration](#demonstration)
+
+## Features
+View and add departmens.
+View, add and update job roles.
+View and add employees.
+
+## Installation
+Clone the repository and install any dependencies with NPM.
+
+## Usage
+Run the application and use the keyboard to navigate and enter data.
+
+## Screenshot
+![Screenshot](./screenshot.png)
+
+## Demonstration
+Here is a [Video Demonstration](https://)
---
+++ db/seeds.sql
@@ -0,0 +1,26 @@
+INSERT INTO department (deptname)
+VALUES
+  ('Frontend'),
+  ('Backend');
+INSERT INTO jobrole (title, salary, department_id)
+VALUES
+  ('Frontend Manager', 34000, 1),
+  ('Backend Manager', 37000, 2),
+  ('Cashier', 20000, 1),
+  ('Greeter', 18000, 1),
+  ('Chef', 25000, 2),
+  ('Prepper', 22000, 2);
+  INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+  ('Michael', 'Jordan', 1, NULL),
+  ('Magic', 'Johnson', 2, NULL),
+  ('Scottie', 'Pippen', 3, 1),
+  ('Clyde', 'Drexler', 3, 1),
+  ('Charles', 'Barkley', 4, 1),
+  ('John', 'Stockton', 4, 1),
+  ('Chris', 'Mullin', 4, 1),
+  ('David', 'Robinson', 5, 2),
+  ('Karl', 'Malone', 5, 2),
+  ('Patrick', 'Ewing', 6, 2),
+  ('Larry', 'Bird', 6, 2),
+  ('Christian', 'Laettner', 6, 2);
