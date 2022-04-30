e40c054499e2100b402bc5a8e51233cae8ce575a
Kaijam Jeffers <faceless128@gmail.com>
2022-04-30 04:00:07 -0400

    add dept

--- index.js
+++ index.js
@@ -12,9 +12,11 @@ db.connect(err => {
 const manageEmployees = () => {
     return inquirer.prompt({
         type: 'list',
+        default: 0,
         name: 'menuOptions',
         message: 'What would you like to do? (Use arrow keys)',
-        choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit']
+        choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit', new inquirer.Separator()],
+        pageSize: 9
     })
     .then(({ menuOptions }) => {
         if (menuOptions === 'View All Employees') {
@@ -54,11 +56,77 @@ const viewEmployees = () => {
             return;
         }
         console.table(rows);
+        // const table = cTable.getTable(rows);
+        // console.log(`${table}`);
+        manageEmployees();
     })
-    manageEmployees();
 };
 
 const addEmployee = () => {
+    const jobQuery = `SELECT title FROM jobrole`;
+    db.query (jobQuery, (err, jobRoles) =>{
+        if (err) {
+            console.log({ error: err.message });
+            return;
+        }
+        console.table(jobRoles);
+    })
+    const managerQuery = `SELECT CONCAT(first_name, ' ', last_name) FROM employee`;
+    db.query (managerQuery, (err, managerChoice) =>{
+        if (err) {
+            console.log({ error: err.message });
+            return;
+        }
+        console.table(managerChoice);
+    })
+    return inquirer.prompt([{
+        type: 'text',
+        name: 'firstName',
+        message: "What is the Employee's first name?",
+        validate: firstNameInput => {
+            if (firstNameInput) {
+                return true;
+            } else {
+                console.log("Please enter a name!");
+                return false;
+            }
+        }
+    },
+    {
+        type: 'text',
+        name: 'lastName',
+        message: "What is the Employee's last name?",
+        validate: lastNameInput => {
+            if (lastNameInput) {
+                return true;
+            } else {
+                console.log("Please enter a name!");
+                return false;
+            }
+        }
+    },
+    {
+        type: 'rawlist',
+        default: 0,
+        name: 'jobrole',
+        message: "What is the Employee's Job Role? (Use arrow keys)",
+        choices: [jobRoles, new inquirer.Separator()],
+        pageSize: 9
+    },
+    {
+        type: 'rawlist',
+        default: 0,
+        name: 'manager',
+        message: "Who is the Employee's Manager? (Use arrow keys)",
+        choices: [managerChoice, new inquirer.Separator()],
+        pageSize: 9
+    }])
+    .then(({ answers }) => {
+        console.table(answers);
+        const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)`;
+        const params = [answers.firstName, answers.lastName, answers.jobrole, answers.manager];
+        manageEmployees();
+    })
 };
 
 const updateEmployee = () => {
@@ -75,11 +143,36 @@ const viewRoles = () => {
             return;
         }
         console.table(rows);
+        manageEmployees();
     })
-    manageEmployees();
 };
 
 const addRole = () => {
+    return inquirer.prompt([{
+        type: 'text',
+        name: 'newrole',
+        message: "What is the New Role?",
+        validate: newroleInput => {
+            if (newroleInput) {
+                return true;
+            } else {
+                console.log("Please enter a Role!");
+                return false;
+            }
+        }
+    }])
+    .then(({ newroledata }) => {
+        const sql = `INSERT INTO jobrole (title, salary, department_id) VALUES (?,?,?)`;
+        const params = newdept;
+        db.query (sql, params, (err, result) => {
+            if (err) {
+                console.log({ error: err.message });
+                return;
+            }
+        })
+        console.log(`Added ${newdept} to Database.`);
+        manageEmployees();
+    })
 };
 
 const viewDepartments = () => {
@@ -90,15 +183,64 @@ const viewDepartments = () => {
             return;
         }
         console.table(rows);
+        manageEmployees();
     })
-    manageEmployees();
 };
 
 const addDepartment = () => {
+    return inquirer.prompt([{
+        type: 'text',
+        name: 'newdept',
+        message: "What is the New Department?",
+        validate: newdeptInput => {
+            if (newdeptInput) {
+                return true;
+            } else {
+                console.log("Please enter a Department!");
+                return false;
+            }
+        }
+    }])
+    .then(({ newdept }) => {
+        const sql = `INSERT INTO department (deptname) VALUES (?)`;
+        const params = newdept;
+        db.query (sql, params, (err, result) => {
+            if (err) {
+                console.log({ error: err.message });
+                return;
+            }
+        })
+        console.log(`Added ${newdept} to Database.`);
+        manageEmployees();
+    })
 };
 
 const quitApp = () => {
-    return false;
+    process.exit();
 };
 
+const splash = () => {
+    console.log(`
+    ______                __                                    
+    / ____/___ ___  ____  / /___  __  _____  ___                 
+   / __/ / __ '__ \\/ __ \\/ / __ \\/ / / / _ \\/ _ \\                
+  / /___/ / / / / / /_/ / / /_/ / /_/ /  __/  __/                
+ /_____/_/ /_/ /_/ .___/_/\\____/\\__, /\\___/\\___/                 
+                /_/            /____/                            
+     __  ___                                                  __ 
+    /  |/  /___ _____  ____ _____ ____  ____ ___  ___  ____  / /_
+   / /|_/ / __ '/ __ \\/ __ '/ __ '/ _ \\/ __ '__ \\/ _ \\/ __ \\/ __/
+  / /  / / /_/ / / / / /_/ / /_/ /  __/ / / / / /  __/ / / / /_  
+ /_/  /_/\\__,_/_/ /_/\\__,_/\\__, /\\___/_/ /_/ /_/\\___/_/ /_/\\__/  
+                          /____/                                 
+    _____            __                                          
+   / ___/__  _______/ /____  ____ ___                            
+   \\__ \\/ / / / ___/ __/ _ \\/ __ '__ \\                           
+  ___/ / /_/ (__  ) /_/  __/ / / / / /                           
+ /____/\\__, /____/\\__/\\___/_/ /_/ /_/                            
+      /____/                        
+    `)
+}
+
+splash();
 manageEmployees();
