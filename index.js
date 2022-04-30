16affe7f797b5e420212b4f08ebafc84e90ddccb
Kaijam Jeffers <faceless128@gmail.com>
2022-04-30 00:28:06 -0400

    added view DB calls

--- index.js
+++ index.js
@@ -10,13 +10,95 @@ db.connect(err => {
 
 // Function to initialize app and start asking questions
 const manageEmployees = () => {
-    inquirer.prompt({
+    return inquirer.prompt({
         type: 'list',
         name: 'menuOptions',
         message: 'What would you like to do? (Use arrow keys)',
-        choices: ['View All Employees', 'Add Employees', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department']
+        choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit']
+    })
+    .then(({ menuOptions }) => {
+        if (menuOptions === 'View All Employees') {
+            viewEmployees()
+        } else if (menuOptions === 'Add Employees' ) {
+            addEmployee()
+        } else if (menuOptions === 'Update Employee Role') {
+            updateEmployee()
+        } else if (menuOptions === 'View All Roles' ) {
+            viewRoles()
+        } else if (menuOptions === 'Add Role' ) {
+            addRole()
+        } else if (menuOptions === 'View All Departments' ) {
+            viewDepartments()
+        } else if (menuOptions === 'Add Department' ) {
+            addDepartment()
+        } else {
+            quitApp()
+        }
     })
 };
 
-// Function call to initialize app
-manageEmployees()
+
+const viewEmployees = () => {
+    const sql = `SELECT e.id, e.first_name, e.last_name, j.title, d.deptname AS department, j.salary, 
+    CONCAT (m.first_name, ' ', m.last_name) AS manager
+    FROM employee e
+    LEFT JOIN jobrole j
+    ON e.role_id = j.id
+    LEFT JOIN department d
+    ON j.department_id = d.id
+    LEFT JOIN employee m
+    ON e.manager_id = m.id`;
+    db.query (sql, (err, rows) =>{
+        if (err) {
+            console.log({ error: err.message });
+            return;
+        }
+        console.table(rows);
+    })
+    manageEmployees();
+};
+
+const addEmployee = () => {
+};
+
+const updateEmployee = () => {
+};
+
+const viewRoles = () => {
+    const sql = `SELECT j.id, j.title, d.deptname AS department, j.salary
+    FROM jobrole j
+    LEFT JOIN department d
+    ON j.department_id = d.id`;
+    db.query (sql, (err, rows) =>{
+        if (err) {
+            console.log({ error: err.message });
+            return;
+        }
+        console.table(rows);
+    })
+    manageEmployees();
+};
+
+const addRole = () => {
+};
+
+const viewDepartments = () => {
+    const sql = `SELECT * FROM department`;
+    db.query (sql, (err, rows) =>{
+        if (err) {
+            console.log({ error: err.message });
+            return;
+        }
+        console.table(rows);
+    })
+    manageEmployees();
+};
+
+const addDepartment = () => {
+};
+
+const quitApp = () => {
+    return false;
+};
+
+manageEmployees();
